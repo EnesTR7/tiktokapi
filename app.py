78ec352859e5520338db9df52221df7885bdc56f
EnesTR7 <120234630+EnesTR7@users.noreply.github.com>
2025-07-31 04:34:51 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import requests
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/gettiktok", methods=["POST"])
+def get_tiktok():
+    tiktok_url = request.json.get("url")
+    if not tiktok_url:
+        return jsonify({"error": "No URL provided"}), 400
+
+    api_url = "https://tiktok-video-no-watermark2.p.rapidapi.com"
+    payload = {
+        'url': tiktok_url,
+        'hd': "0"
+    }
+
+    headers = {
+        'User-Agent': "okhttp/3.12.0",
+        'Accept-Encoding': "gzip",
+        'x-rapidapi-host': "tiktok-video-no-watermark2.p.rapidapi.com",
+        'x-rapidapi-key': "b7a226349dmshe6962f8400d6eedp1d6c1ejsne5229da5f4cc"
+    }
+
+    response = requests.post(api_url, data=payload, headers=headers)
+    return jsonify(response.json())
+
+if __name__ == "__main__":
+    app.run(debug=True)
